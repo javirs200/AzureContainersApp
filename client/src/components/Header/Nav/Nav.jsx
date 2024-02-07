0bed64c941b75f6a87af18b3a63b4c523d779dee
javi <fordrsmax@gmail.com>
2024-02-07 17:40:09 +0100

    sync client to new model

--- client/src/components/Header/Nav/Nav.jsx
+++ client/src/components/Header/Nav/Nav.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from 'react-router-dom'
 import foto from "../../../assets/favicon.svg";
+import { UserContext } from "../../../context/userContext";
 
 const Nav = () => {
+  const { logged } = useContext(UserContext)
   return (
     <nav>
       <img src={foto} alt="logo" className="logo_nav" />
-    <ul>
-      <li><Link className={'link'} to='/home'>Inicio</Link></li>
-      <li><Link className={'link'} to='/offer'>Propuestas</Link></li>
-      <li><Link className={'link'} to='/users'>Usuarios</Link></li>
-      <li>Perfil</li>
-    </ul>    
+      {logged ?
+        <ul>
+          <li><Link className={'link'} to='/home'>Inicio</Link></li>
+          <li><Link className={'link'} to='/offer'>Propuestas</Link></li>
+          <li><Link className={'link'} to='/users'>Usuarios</Link></li>
+        </ul>
+        : 
+        <ul>
+          <li><Link className={'link'} to='/users'>Usuarios</Link></li>
+          <li><Link className={'link'} to='/login'>login</Link></li>
+        </ul>
+        }
     </nav>
   );
 };
