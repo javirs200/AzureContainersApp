ce3143801a5891dfb7c17951be1ee92de3313ee5
rsmax <fordrsmax@gmail.com>
2024-02-21 18:51:06 +0100

    fix landing buttons navigation

--- client/src/components/Main/Landing/Landing.jsx
+++ client/src/components/Main/Landing/Landing.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
 
 const Landing = () => {
+
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>Landing</h1>
-      <Button variant="contained">
-        <Link className={'link'} to={`/login`}>login</Link>
+      <Button variant="contained" onClick={()=>{navigate("/login")}}>
+        Login
       </Button>
-      <Button variant="text">
-        <Link className={'link'} to={`/Register`}>Register</Link>
+      <Button variant="text" onClick={()=>{navigate("/register")}}>
+        Register
       </Button>
     </>
   );
