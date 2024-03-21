b0c650643fe0f30efef544d67fc410919ce90b13
javi <fordrsmax@gmail.com>
2024-03-21 17:57:32 +0100

    add comparative version

--- PiFiles/MainStation/ConcurrentMain.py
+++ PiFiles/MainStation/ConcurrentMain.py
@@ -1,5 +1,7 @@
 import multiprocessing
+import threading
 import time
+import asyncio
 
 def count_up():
     count = 0
@@ -7,6 +9,16 @@ def count_up():
         count = count + i
 
 def count_down():
+    count = 0
+    for i in range(100000000):
+        count = count + i
+
+async def count_up_async():
+        count = 0
+        for i in range(100000000):
+            count = count + i
+
+async def count_down_async():
     count = 0
     for i in range(100000000):
         count = count + i
@@ -28,4 +40,36 @@ if __name__ == "__main__":
 
     end_time = time.time()
 
-    print(f"Time taken: {end_time - start_time} seconds")
+    start_time2 = time.time()
+
+    # Create two threads, each running a CPU-bound task
+    process1 = multiprocessing.Process(target=count_up)
+    process2 = multiprocessing.Process(target=count_down)
+
+    # Start both threads
+    process1.start()
+    process2.start()
+
+    # Wait for both threads to finish
+    process1.join()
+    process2.join()
+
+    end_time2 = time.time()
+
+    start_time3 = time.time()
+
+    async def main_async():
+        # Create two tasks, each running a CPU-bound task
+        task1 = asyncio.create_task(count_up())
+        task2 = asyncio.create_task(count_down())
+
+        # Wait for both tasks to finish
+        await asyncio.gather(task1, task2)
+
+    asyncio.run(main_async())
+
+    end_time3 = time.time()
+    
+    print(f"Time taken multiprocesing: {end_time - start_time} seconds")
+    print(f"Time taken multitread: {end_time2 - start_time2} seconds")
+    print(f"Time taken asyncio: {end_time3 - start_time3} seconds")
