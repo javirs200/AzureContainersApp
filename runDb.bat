d135011d4350129a18730fe1ef6346cf1297eb8f
javi <fordrsmax@gmail.com>
2024-04-17 18:22:08 +0200

    add run db bat script

---
+++ runDb.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Check if the Docker Compose database is already running
+docker-compose ps -q db > nul
+if %errorlevel% equ 0 (
+    echo Stopping and removing existing Docker Compose database...
+    docker-compose -f "docker-compose-DB.yml" down
+)
+
+REM Start the Docker Compose database
+docker-compose -f "docker-compose-DB.yml" up -d db
+
+REM Wait for the database to start
+echo Waiting for the database to start...
+:loop
+
+REM run the init script
+cd ./server
+npm run init
