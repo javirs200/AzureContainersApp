e3d80b803e14aa1880311b43efcecda1c53d34fb
rpi <fordrsmax@gmail.com>
2024-04-30 16:27:50 +0200

    woking esp32 socket

--- PiFiles/MainStation/utills/socketServer.py
+++ PiFiles/MainStation/utills/socketServer.py
@@ -1,27 +1,30 @@
 import socket
+from multiprocessing import Process
 
 class Server:
     def __init__(self, address='', port=12345):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = (address, port)
         self.server_socket.bind(self.server_address)
-        self.server_socket.listen(1)
+        self.server_socket.listen()
         print('Server is listening on {}:{}'.format(*self.server_address))
 
     def start(self,timers:dict,times:dict):
+
+        def recive(client_socket):
+            while True:
+                data = client_socket.recv(1024)
+                if data : 
+                    message = data.decode()
+                    print('Received data: {}'.format(message))
+
         try:
             while True:
                 client_socket, client_address = self.server_socket.accept()
                 print('Received connection from {}:{}'.format(*client_address))
+                p = Process(target=recive, args=(client_socket,))
+                p.start()
                 
-                data = client_socket.recv(1024)
-                message = data.decode()
-                print('Received data: {}'.format(message))
-                
-
-                response = 'Hello from the server!'
-                client_socket.sendto(response.encode(), client_address)
-
         except Exception as e:
             print('An error occurred:', str(e))
 
