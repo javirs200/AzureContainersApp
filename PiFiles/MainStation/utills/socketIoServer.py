c6cb1368632f3f018c1cb9c699dd8f0f5512b35c
javi <fordrsmax@gmail.com>
2024-05-03 19:36:26 +0200

    start event listener

--- PiFiles/MainStation/utills/socketIoServer.py
+++ PiFiles/MainStation/utills/socketIoServer.py
@@ -12,16 +12,25 @@ class IoServer:
         @self.sio.event
         def connect(sid, environ):
             print('connect ', sid)
-            while True:
-                if len(self.times) > 0:
-                    message = self.times.popitem() # pop element of a dict
-                    print('sending message to ',sid,' ->', message)
-                    self.sio.emit('new_time', {'time': message}, room=sid)
-                self.sio.sleep(1)
-                
+           
         @self.sio.event
         def disconnect(sid):
             print('disconnect ', sid)
+
+        @self.sio.event
+        def control(sid, data):
+            if data['command'] == 'start':
+                print('start')
+                while True:
+                    if len(self.times) > 0:
+                        message = self.times.popitem() # pop element of a dict
+                        print('sending message to ',sid,' ->', message)
+                        self.sio.emit('new_time', {'time': message}, room=sid)
+                    self.sio.sleep(1)
+            elif data['command'] == 'stop':
+                print('stop')
+                self.times.clear()
+                    
 
     def start(self,times: dict):
         self.times = times
