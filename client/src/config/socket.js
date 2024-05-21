6deb61573cbdbd98a96caf30f33170f725027e68
javi <fordrsmax@gmail.com>
2024-05-21 16:59:51 +0200

    change raspberry pi url on client

--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://raspberry.mshome.net:3000';
+// const URL = 'http://raspberry.mshome.net:3000';
+const URL = 'http://raspberry:3000';
 
 const socket = io(URL, {
   autoConnect: false,
