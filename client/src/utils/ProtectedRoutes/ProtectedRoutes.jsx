2d2cfe0146b018c4004a9e26fc560e04bd9e39c1
javi <fordrsmax@gmail.com>
2024-05-21 13:06:49 +0200

    remove al unrelated files to esp32

--- client/src/utils/ProtectedRoutes/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ProtectedRoutes = ({ component, logged }) => {
-    return (
-        logged ? component : <div><span>Please log in first</span></div>
-    )
-};
-
-export default ProtectedRoutes;
