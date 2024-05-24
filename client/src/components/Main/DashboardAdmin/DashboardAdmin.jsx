407908b68dab0c0d81f4ae29d1de9abdc7a82c75
Ubuntu <azureuser@CronosTimer.3wmuvuv22vlexbyf50j0lnhpyb.ax.internal.cloudapp.net>
2024-05-24 13:28:12 +0000

    add user inscriptions funcionality to admin user

--- client/src/components/Main/DashboardAdmin/DashboardAdmin.jsx
+++ client/src/components/Main/DashboardAdmin/DashboardAdmin.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Logout from "../Logout";
+import EventsList from "../Dashboard/EventsList";
+import MyParticipations from "../Dashboard/MyParticipations";
 
 const Dashboard = () => {
 
   return (
     <>
       <h1>Dashboard Admin</h1>
+      <section className="DashboardContent">
+        <EventsList />
+        <MyParticipations />
+      </section>
       <Logout />
     </>
   );
