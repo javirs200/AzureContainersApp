914b394e131b360f51e929594ba89f60d7853fcd
javi <fordrsmax@gmail.com>
2024-06-19 12:34:42 +0200

    add auto population script on first load

--- server/config/db_pgsql.js
+++ server/config/db_pgsql.js
@@ -9,10 +9,27 @@ const db = new Sequelize(process.env.SQL_NAME, process.env.SQL_USER, process.env
     logging:false
   });
 
+const checkDatabaseContent = async () => {
+    console.log('check database content');
+    const users = await db.users.findAll()
+    if(users.length == 0){
+        console.log('no users found ,add default data');
+        console.log('data clearing');
+        res = require('../scripts/clearDatabase');
+        console.log('data population');
+        res = require('../scripts/populateDataBase');
+    }else{  
+        console.log('users found');
+    }
+    console.log('database content checked');
+}
+
 const connectSQL = async () => {
     try {        
         await db.authenticate();
         console.log('PostgreSQL database connected...');
+
+        console.log('Sequiellize db models sincronization');
 
         console.log('add models to db');
         db.users = require('../models/users.model')
@@ -33,6 +50,8 @@ const connectSQL = async () => {
         db.sync()
         
         console.log('db ready');
+
+        checkDatabaseContent();
 
     } catch (error) {
         console.error('Unable to connect to SQL database:', error);
